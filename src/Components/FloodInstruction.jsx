76560ebc8aae651996d74697f5d019cc542c7a81
mitchFraser11 <102576736@student.swin.edu.au>
2024-04-30 10:30:26 +1000

    Added flood instruction information

--- src/Components/FloodInstruction.jsx
+++ src/Components/FloodInstruction.jsx
@@ -2,8 +2,27 @@ function FloodInstruction() {
   return (
     <>
       <div style={{ background: '#2b838f', width: '100%', height: '1000px' }}>
-        <h2>Flood Instruction Component</h2>
-        <p>This is the Flood Instruction component.</p>
+        <h2>Flood Instruction</h2>
+        <p>The following is to be used as a guide only. Always follow current advice given by emergency services.</p>
+        <p>Please ensure you have a emergency kit prepared and easily accessible. Make sure you have enough supplies in your emergency kit to last a few days. Somethings that should be in your kit are:</p>
+        <ol>
+          <li>First Aid Kit</li>
+          <li>Non-perishable foods</li>
+          <li>Bottled Water</li>
+          <li>Flashlights and spare batteries</li>
+          <li>Any necessary medication that you require to take</li>
+        </ol>
+        <p>Avoid walking through flooded streets or any body of moving water. The water may appear to be moving slowly but could drag you away very quickly.</p>
+        <p>After a flood has occured, wait for the advice of emergency services before leaving/entering structures incase they have been weakened during the flood.</p>
+        <p>Make sure devices such as mobile phones are fully charged and that you have portable charges available for them. This is to ensure you can reach emeergency services and receive any updates about the flood. </p>
+        <p>Stay informed across the duration of the disaster by using radio, TV or mobile apps to be able to receive the lastest weather reports and warnings advised by officials.</p>
+      <p>Fortifying your home can also help safeguard belongings and your family. Some good recommendations about fortifying your home are:</p>
+      <ol>
+        <li>Remove or secure any loose objects from balconies or roofs</li>
+        <li>Lock all exterior facing windows and doors</li>
+        <li>Installing bars or reinforced typhoon shutters can also assist with minimizing damage</li>
+        <li>If you cant get shutters, you can use boards and tape to board up windows and doors</li>
+      </ol>
       </div>
     </>
   )
