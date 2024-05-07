528f7716d0e6ee9eb67c436c92778a013639f580
mitchFraser11 <102576736@student.swin.edu.au>
2024-05-07 14:53:07 +1000

    Added basic structure and information to email settings. Need to add functionality and improve structure

---
+++ src/Components/EmailSetting.jsx
@@ -0,0 +1,14 @@
+function EmailSetting() {
+    return (
+      <>
+        <div style={{ background: '#2b838f', width: '100%', height: 'auto' }}>
+          <h2>Email Notification Settings</h2>
+          <p>Below is the setting to enable email notifications about warnings for a district of your choosing. You can also change your selected district at any time by submitting the following section again.</p>
+          <p>Form will go here</p>
+          <p>Please note you should not just rely on our notifications. While we strive to provide up-to-date notifications, you should always listen to current information being provided by emergency services.</p>
+        </div>
+      </>
+    )
+  }
+    
+  export default EmailSetting
