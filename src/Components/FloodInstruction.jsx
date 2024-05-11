183627dd7b4d62f00005847256f8dd5b81e70b03
Kwan Ho Leung <enzokwan@KwandeMacBook-Pro.local>
2024-05-11 18:25:12 +1000

    tune vid size

--- src/Components/FloodInstruction.jsx
+++ src/Components/FloodInstruction.jsx
@@ -3,7 +3,10 @@ function FloodInstruction() {
     <>
       <div style={{ background: '#2b838f', width: '100%', height: 'auto' }}>
         <h2 style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>Flood Instruction</h2>
-        <iframe width="1104" height="693" src="https://www.youtube.com/embed/eR55MT6r9xU" title="Flooding 101: A Complete Guide to Flood Planning and Preparedness" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+          <iframe width="552" height="346" src="https://www.youtube.com/embed/eR55MT6r9xU" title="Flooding 101: A Complete Guide to Flood Planning and Preparedness" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
         <p style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>The above is to be used as a guide only. Not all suggestions will be relevant or applicable to you.</p>
         <strong style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}> Always follow current advice given by emergency services.</strong>
         
