8676c7256f92cc005d73193882902e404095fb9b
Kwan Ho Leung <enzokwan@KwandeMacBook-Pro.local>
2024-05-12 12:44:30 +1000

    added Flood Probability to the tuned weather app and flood instruction

--- src/Components/FloodInstruction.jsx
+++ src/Components/FloodInstruction.jsx
@@ -15,17 +15,17 @@ function FloodInstruction() {
           <iframe width="552" height="346" src="https://www.youtube.com/embed/eR55MT6r9xU" title="Flooding 101: A Complete Guide to Flood Planning and Preparedness" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
 
-        <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'start', paddingLeft: '40px', paddingTop: '40px'}}>
-          <h2>Stay Safe</h2>
+        <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'start', padding: '40px',}}>
+          <h2>1. Stay Safe</h2>
           <p>Remain indoors unless it is unsafe to do so. Avoid low-lying areas and regions prone to flooding. If you are currently in a flood-prone area, consider relocating to higher ground.</p>
 
-          <h2>Stay Informed</h2>
+          <h2>2. Stay Informed</h2>
           <p>Continuously monitor local news updates and the <a href="https://www.hko.gov.hk/en/index.html">Hong Kong Observatory's official website</a> for the latest information and advisories.</p>
 
-          <h2>Evacuation Readiness</h2>
+          <h2>3. Evacuation Readiness</h2>
           <p>Familiarize yourself with the nearest evacuation points. Be prepared to evacuate promptly if instructed by emergency services. Keep essential items such as medications, important documents, and necessary personal items ready for quick departure.</p>
 
-          <h2>Additional Safety Tips</h2>
+          <h2>4. Additional Safety Tips</h2>
           <ul>
             <li>Avoid electrical equipment and outlets near water.</li>
             <li>Do not attempt to wade or drive through floodwaters.</li>
@@ -33,12 +33,12 @@ function FloodInstruction() {
             <li>Ensure your mobile devices are fully charged.</li>
           </ul>
 
-          <h2>Emergency Contacts</h2>
+          <h2>5. Emergency Contacts</h2>
           <p>For emergencies, dial 999.</p>
         </div>
 
 
-        <div style={{display: 'flex', justifyContent: 'center', paddingTop: '60px'}}>
+        <div style={{display: 'flex', justifyContent: 'center', paddingTop: '20px'}}>
           <h1>Evacuation Points</h1>
         </div>
         <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'start', paddingLeft: '40px',}}>
--- src/Components/FloodWarningDisplay.jsx
+++ src/Components/FloodWarningDisplay.jsx
@@ -1,18 +1,129 @@
+import { useState } from "react";
 import { Grid } from "@mui/material";
+import use1hrFloodPercentage2 from '../Hooks/use1hrFloodPercentage2';
+import WaterIcon from '@mui/icons-material/Water';
 
 function FloodWarningDisplay() {
-  return (
-    <>
-      <Grid container sx={{background: '#2b838f', width: '100%', height: '500px', borderRadius: '8px'}}>
-      {/* <div style={{ background: '#2b838f', width: '100%', height: '500px' }}> */}
-      <div style={{width: '100%', height: '100%', paddingLeft: '20px'}}>
-        <h2>Flood Warning Display Component</h2>
-        <p>This is the Flood Warning Display component.</p>
+
+  const [ isWarningRelease, setIsWarningRelease ] = useState(false)
+
+  const { currentPercentage, loading, error } = use1hrFloodPercentage2();
+
+  if (loading) {
+    return (
+      <div style={{ background: '#212121', width: '100%', height: '500px', borderRadius: '8px' }}>
+        <h2>Loading...</h2>;
       </div>
-      {/* </div> */}
+    )
+  }
+
+  if (error) {
+    return (
+      <div style={{ background: '#212121', width: '100%', height: '500px', borderRadius: '8px' }}>
+        <h2>Error: </h2>;
+        <p>{error}</p>
+      </div>
+    )
+  }
+
+  if (currentPercentage === null || currentPercentage === undefined ) {
+    return (
+      <div style={{ background: '#212121', width: '100%', height: '500px', borderRadius: '8px' }}>
+        <h2>No data available.</h2>;
+      </div>
+    )
+  }
+
+  const returnColor = (num) => {
+    if (num >= 0 && num <= 29) {
+      return "#1de9b6"
+    }
+    if (num >= 30 && num <= 49) {
+      return "#ffcf33"
+    }
+    if (num >= 50 && num <= 99) {
+      return "#ff9100"
+    }
+    if (num >= 100) {
+      return "#ff1744"
+    }
+    if (num <0 ) {
+      return "#636363"
+    }
+  }
+
+  const displayColor = returnColor(currentPercentage)
+
+  const DisplayPercentage = (num) => {
+    if (num >= 80) {
+      return num - 10
+    } else {
+      return num
+    }
+  }
+
+
+  if (isWarningRelease === false){
+    return (
+      <>
+        <Grid container sx={{background: '#212121', width: '100%', height: '500px', borderRadius: '8px'}}>
+
+        <div style={{display: 'flex', justifyContent: 'center', width: '100%'}}><h2>Flood Probability</h2></div>
+
+        <div style={{width: '100%', height: '100%', paddingLeft: '20px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+          
+              <div style={{
+                width: '200px',
+                height: '200px',
+                borderRadius: '8px',
+                backgroundColor: displayColor,
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginBlockEnd: '10em'
+              }}>
+                <p style={{ 
+                  fontSize: '50px',
+                  fontWeight: 'bold',
+                  marginBlockStart: '0',
+                  marginBlockEnd: '0'
+                }}>
+                    {`${DisplayPercentage(currentPercentage)}%`}
+                </p>
+            </div>
+
+        </div>
+
+        </Grid>
+      </>
+    )
+  }
+
+
+  if (isWarningRelease === true) {
+    return(
+      <>
+      <Grid container sx={{background: '#d61334', width: '100%', height: '500px', borderRadius: '8px'}}>
+        <div style={{width: '100%', height: '100%', paddingLeft: '20px', paddingRight: '20px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+          <p style={{ 
+            fontSize: '45px',
+            fontWeight: 'bold',
+            marginBlockStart: '0',
+            marginBlockEnd: '0',
+            textAlign: 'center'
+          }}>
+            FLOOD WARNING RELEASED
+          </p>
+          <WaterIcon style={{fontSize: '120px'}}/>
+          <p style={{marginBlockEnd: '0px'}}>Stay safe, check the instruction</p>
+          <p style={{marginBlockStart: '10px'}}>follow advice from emergency services</p>
+        </div>
       </Grid>
-    </>
-  )
+      </>
+    )
+  }
+
+
 }
   
 export default FloodWarningDisplay
